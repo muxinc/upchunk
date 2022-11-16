1810751c8e47f6b47e800d567a39b194aa038b5e
Christian Pillsbury <cpillsbury@mux.com>
2022-11-16 08:14:06 -0800

    chore: move tests to test dir. cleanup test config format.

--- test/web-test-runner.config.mjs
+++ test/web-test-runner.config.mjs
@@ -3,21 +3,18 @@ import { importMapsPlugin } from '@web/dev-server-import-maps';
 
 export default {
   nodeResolve: true,
-  /** @TODO Final home for tests. Uncomment before PR merge */
-  // files: ['test/**/*.spec.js', 'test/**/*.spec.ts'],
-  /** @TODO Interim home for tests to make diff/code review easier. Remove before PR merge */
-  files: ['src/**/*.spec.js', 'src/**/*.spec.ts'],
+  files: ['test/**/*.spec.js', 'test/**/*.spec.ts'],
   plugins: [
     importMapsPlugin({
       inject: {
         importMap: {
           imports: {
-            'xhr': './test/dist/xhr.mjs',
+            xhr: './test/dist/xhr.mjs',
             'xhr-mock': './test/dist/xhr-mock.mjs',
           },
         },
       },
     }),
-    esbuildPlugin({ ts: true })
+    esbuildPlugin({ ts: true }),
   ],
 };
